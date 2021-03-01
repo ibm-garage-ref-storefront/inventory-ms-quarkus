5f1ff53f847bbe8fff498153158671f409ad61da
Hemankita Perabathini <phemankita@gmail.com>
2021-03-01 17:10:46 +0530

    Enabling tracing

--- src/main/java/ibm/cn/application/InventoryResource.java
+++ src/main/java/ibm/cn/application/InventoryResource.java
@@ -7,11 +7,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.opentracing.Traced;
+import org.jboss.logging.Logger;
+
 import ibm.cn.application.model.Inventory;
 import ibm.cn.application.repository.InventoryRepository;
 
 @Path("/micro/inventory")
 public class InventoryResource {
+	
+	private static final Logger LOG = Logger.getLogger(InventoryResource.class);
 	
 	private final InventoryRepository inventoryRepository;
 
@@ -22,12 +27,15 @@ public class InventoryResource {
 	@GET
 	@Path("/resource")
     public String getRequest() {
+		LOG.info("/resource endpoint");
         return "InventoryResource response";
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Traced
     public List<Inventory> getInventoryDetails() {
+    	LOG.info("/inventory endpoint");
     	return inventoryRepository.getInventoryDetails();
     }
 }
